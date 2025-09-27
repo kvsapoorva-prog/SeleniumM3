ff246f21142abf8dbd16ccfbe25265ffa253d2b1
kvsap <kvsap@Apoorva>
2025-09-27 11:17:52 +0530

    testing

--- com.ShopperStack_Ap/src/main/java/ShopperStack_GenericUtility/BaseTest.java
+++ com.ShopperStack_Ap/src/main/java/ShopperStack_GenericUtility/BaseTest.java
@@ -57,8 +57,13 @@ public class BaseTest {
 	public void beforeClass() throws IOException {
 		System.out.println("@BeforeClass ------------ Browser Launched");
 
-		String browser = fileUtility.readDataFromPropertyFile("browserName");
-		String url = fileUtility.readDataFromPropertyFile("url");
+	//	String browser = fileUtility.readDataFromPropertyFile("browserName");
+	//	String url = fileUtility.readDataFromPropertyFile("url");
+		
+		String browser = System.getProperty("browserName");
+		String url = System.getProperty("url");
+		
+		
 		if (browser.equalsIgnoreCase("chrome")) {
 			driver = new ChromeDriver();
 		} else if (browser.equalsIgnoreCase("edge")) {
